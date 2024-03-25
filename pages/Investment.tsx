2b586bf3b8d8bcf51e8be8c64b21c262c0ab79a2
mhmAdmin1122 <hasaanad12min@gmail.com>
2024-03-25 16:05:14 +0500

    Investment layout

---
+++ pages/Investment.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PageLayout from "./components/PageLayout";
+import Head from "next/head";
+
+const InvestmentPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Investment - Itavi Mining</title>
+      </Head>
+      <PageLayout>
+        <div>Investment</div>
+      </PageLayout>
+    </div>
+  );
+};
+
+export default InvestmentPage;
