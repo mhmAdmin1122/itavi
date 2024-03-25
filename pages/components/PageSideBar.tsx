8c5075300f97854b27596a4bee37ed7a3343561f
mhmAdmin1122 <hasaanad12min@gmail.com>
2024-03-25 06:22:49 +0500

    disclaimer page ready to view

--- pages/components/PageSideBar.tsx
+++ pages/components/PageSideBar.tsx
@@ -10,11 +10,11 @@ const PageSideBar = () => {
         <Image src={logo} alt='logo-pic-itavi' width={140} height={120} className='logo-pic-side' />
       </Link>
       <div className="sidebar-tabs h-[75%] flex flex-col gap-2 font-normal text-white text-left items-end pt-[55px] pr-4">
-        <Link href="/contact">Contact</Link>
-        <Link href="/faqs">FAQs</Link>
-        <Link href="/disclaimer">Disclaimer</Link>
-        <Link href="/terms">Terms & Conditions</Link>
-        <Link href="/privacy">Privacy Policy</Link>
+        <Link className='shadow-text' href="/contact">Contact</Link>
+        <Link className='shadow-text' href="/faqs">FAQs</Link>
+        <Link className='shadow-text' href="/disclaimer">Disclaimer</Link>
+        <Link className='shadow-text' href="/terms">Terms & Conditions</Link>
+        <Link className='shadow-text' href="/privacy">Privacy Policy</Link>
       </div>
     </div>
   )
