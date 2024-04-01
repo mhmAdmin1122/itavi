75fd9e2bf3976e548a5ac3b88049845bdc74653c
mhmAdmin1122 <hasaanad12min@gmail.com>
2024-04-01 19:00:15 +0500

    few more

--- pages/components/PageSideBar.tsx
+++ pages/components/PageSideBar.tsx
@@ -17,7 +17,6 @@ const PageSideBar = () => {
         <Image src={logo} alt='logo-pic-itavi' width={140} height={120} className='logo-pic-side' />
       </Link>
       <div className="sidebar-tabs h-[75%] flex flex-col gap-2 font-normal text-white text-left items-end pt-[55px] pr-4">
-        <Link className='shadow-text' href="/contact">Contact</Link>
         <Link className='shadow-text' href="/faqs">FAQs</Link>
         <Link className='shadow-text' href="/disclaimer">Disclaimer</Link>
         <Link className='shadow-text' href="/terms">Terms & Conditions</Link>
@@ -27,15 +26,11 @@ const PageSideBar = () => {
         <RxHamburgerMenu  />
       </div>
       {showTabs && <div className="mobile-sidebar-tabs absolute top-[60px] left-0 w-full bg-[#0000009a] flex flex-col gap-2 font-normal text-white text-right items-end p-4 text-[14px]">
-        <Link className='shadow-text' href="/contact">Contact</Link>
         <Link className='shadow-text' href="/faqs">FAQs</Link>
         <Link className='shadow-text' href="/disclaimer">Disclaimer</Link>
         <Link className='shadow-text' href="/terms">Terms & Conditions</Link>
         <Link className='shadow-text' href="/privacy">Privacy Policy</Link>
       </div>}
-      {/* <div className='absolute router-back-link-toster'>
-        <RouterBack />
-      </div> */}
     </div>
   )
 }
