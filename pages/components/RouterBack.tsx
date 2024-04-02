fcea170f584e9eccc1cd3bd5d799e081bc8c583a
mhmAdmin1122 <hasaanad12min@gmail.com>
2024-04-02 21:16:15 +0500

    few more

--- pages/components/RouterBack.tsx
+++ pages/components/RouterBack.tsx
@@ -5,9 +5,10 @@ const RouterBack = () => {
   
   return (
     <Link href="/#about"
-      className="slider-button fixed top-[50%] max-[425px]:top-[115%] left-2 bg-[#00000081] text-[#fff] font-bold px-4 py-2 text-xl hover:bg-[#000000]"
+      className="slider-button flex items-center gap-2 fixed top-[50%] max-[425px]:top-[115%] left-2 bg-[#00000081] text-[#fff] font-bold px-4 py-2 text-xl hover:bg-[#000000]"
     >
       <FaAngleLeft />
+      <p className="cursor-pointer">Home</p>
     </Link>
   );
 };
